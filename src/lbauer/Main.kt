626a16832c98cbf38b5dd677e706b9db94bb24df
lutzuca <lutzuca@gmx.de>
2021-05-20 15:29:26 +0200

    for loop kennenlernen

--- src/lbauer/Main.kt
+++ src/lbauer/Main.kt
@@ -3,21 +3,15 @@ package lbauer
 import java.util.*
 
 fun main(args: Array<String>){
-    val scan = Scanner(System.`in`)
 
-    when (scan.nextLine().toInt()) {
-        1 ->
-            println("x = 1")
-        2 ->
-            println("x = 2")
-        3,4 ->
-            println("x is either 3 or 4")
-        5 -> {
-            print("x = ")
-            println(5)
-        }
-        else ->
-            println("Hello!")
+    var liste = ArrayList<Int>();
 
-    }
+    liste.add(1)
+    liste.add(2)
+    liste.add(3)
+    liste.add(4)
+
+    for(item in liste)
+        println(item)
+
 }
